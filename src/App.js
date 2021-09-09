c34b716487c7225fb12a5f317c4b2a77aef908f7
Mateus Gonçalves Franco <mattfran67@gmail.com>
2021-09-09 14:45:52 -0300

    Atualizado componente App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
+import { useState } from "react";
+import { getRandomMsg } from "./fakeApi/getRandomMsg";
+
 const App = () => {
-  return ;
+  const [message, setMessage] = useState({
+    text: '',
+    loading: false
+  });
+
+  const getMessage = () => {
+    setMessage({ loading: true });
+    getRandomMsg().then(response => {
+      setMessage({ text: response.text, loading: false });
+    });
+  };
+
+  return (
+    <>
+      <button onClick={getMessage}>
+        Frase do Dia
+      </button>
+      <p>{message.loading ? 'Carregando...' : message.text}</p>
+    </>
+  );
 }
 
 export default App;
